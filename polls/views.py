e4ab39e4937f8dd6c4d447c2c1f59074ee50c3bf
vanshikamadan <vanshikamadan97@yahoo.com>
2017-06-12 16:01:53 +0530

    third commit

--- polls/views.py
+++ polls/views.py
@@ -33,7 +33,8 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
+        selected_choice.votes += selected_choice.votes
+	
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
